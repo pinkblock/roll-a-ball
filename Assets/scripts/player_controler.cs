96129b8640b04fd43ac4e216fe6e1a9f99da8cfa
Seith Miller <seithmiller@gmail.com>
2015-11-02 22:16:33 -0500

    the ball moves

---
+++ Assets/scripts/player_controler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_controler : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movment = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movment * speed);
+	}
+}
